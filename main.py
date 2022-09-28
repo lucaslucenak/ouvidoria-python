799a70bda937ceeb9665c2bbaeb892cfaa043722
Lucas Lucena <lucas.lucenak@gmail.com>
2022-09-28 01:22:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from services import UsuarioService, ElogioService, ReclamacaoService
 usuarioService = UsuarioService.UsuarioService()
 elogioService = ElogioService.UsuarioService()
 reclamacaoService = ReclamacaoService.ReclamacaoService()
-
